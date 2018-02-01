34c6ec68aa944ac163c9eefb12b85d70c7e5828b
Madhur Garg <madhurgarg71@gmail.com>
2018-02-01 14:23:13 +0530

    Update hipbar-server.js

--- src/server/hipbar-server.js
+++ src/server/hipbar-server.js
@@ -3,13 +3,13 @@ const config = require('./config')
 
 // console.log(config);
 
-function HipbarServer(rootApp) {
+function YourServer(rootApp) {
   this.rootApp = rootApp
   this.httpServer = null
   this.config = config
 }
 
-HipbarServer.prototype.start = function start(externalApp) {
+YourServer.prototype.start = function start(externalApp) {
   const app = externalApp || this.rootApp
   // console.log(config);
   this.httpServer = app.listen(config.server.port)
@@ -17,12 +17,12 @@ HipbarServer.prototype.start = function start(externalApp) {
   this.httpServer.on('listening', () => this.logStartMessages())
 }
 
-HipbarServer.prototype.logStartMessages = function logStartMessages() {
+YourServer.prototype.logStartMessages = function logStartMessages() {
   const env = process.env.NODE_ENV
 
-  console.log(`${chalk.blue('Hipbar server creation...')} ${chalk.underline('Successfull')}`)
+  console.log(`${chalk.blue('Your server creation...')} ${chalk.underline('Successfull')}`)
   console.log(`${'Server started in'} ${chalk.white.italic(env)} mode on port ${chalk.white.italic(config.server.port)}`)
   console.log('Press CTRL-C to shutdown\n\n')
 }
 
-module.exports = HipbarServer
+module.exports = YourServer
